692c5f6ea378d0551fb71f3b525acd2bcbca4ce2
Rovie Matito <roviematito1517@gmail.com>
2026-08-25 09:37:18 +0800

    generated elements

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.getElementById("profile-container");
+
+const card = document.createElement('div');
+const picture = document.createElement('img');
+const headingName = document.createElement('h2');
+const bio = document.createElement('p');
+
