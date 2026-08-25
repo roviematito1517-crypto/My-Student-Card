ee6461be3e2afa5bf6db383e7389039434d66727
Rovie Matito <roviematito1517@gmail.com>
2026-08-25 11:12:39 +0800

    populate and append

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ const picture = document.createElement('img');
 const headingName = document.createElement('h2');
 const bio = document.createElement('p');
 
+card.className = 'profile-card';
+
+picture.src = 'mypicture.jpg';
+picture.alt = 'Student Photo';
+picture.className = 'profile-picture';
+
+headingName.textContent = 'Rovie Matito'
+bio.textContent = 'Bachelor of Science in Information System Student 2nd Year';
+
+card.appendChild(picture);
+card.appendChild(headingName);
+card.appendChild(bio);
+
+container.appendChild(card);
+
+
+
